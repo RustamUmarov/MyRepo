613599dc9907d604b320fa867209c66725918b68
RustamUmarov <rumarov@medyear.org>
2016-10-12 16:30:55 -0500

    My commit!

---
+++ Product/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Program();
+            var number1 = a.ToList("62354623458723642");
+            var number2 = a.ToList("82648234723643287");
+            
+        }
+        public List<Int16> ToList(String number) {
+            List<Int16> numberList = new List<Int16>();
+            for (int i = 0; i < number.Length; i++) 
+            {
+                var sub1 = number.Substring(0, 1);
+                numberList.Add(Int16.Parse(sub1));
+            }
+            return numberList;
+        }
+    }
+}
